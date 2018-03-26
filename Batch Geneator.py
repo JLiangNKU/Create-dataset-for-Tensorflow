012c1ddf4d481b528ac75fb20dcf2711d49c9e0f
Mr.  Little Zhu <zhumavip@163.com>
2018-03-26 15:41:41 +0800

    Update Batch Geneator.py

--- Batch Geneator.py
+++ Batch Geneator.py
@@ -57,44 +57,7 @@ def read_tfRecord(file_tfRecord):
     return image,label
 
 
-if __name__ == '__main__':
-    getTrianList()
-    dataroad = "/Users/zhuxiaoxiansheng/Desktop/Yaledata.txt"
-    outputdir = "/Users/zhuxiaoxiansheng/Desktop/Yaledata"
 
-    trainroad = trans2tfRecord(dataroad,outputdir)
-
-    traindata,trainlabel = read_tfRecord(trainroad)
-
-    image_batch,label_batch = tf.train.shuffle_batch([traindata,trainlabel],
-                                            batch_size=1,capacity=2000,min_after_dequeue = 1000) 
-        
-    with tf.Session() as sess:
-        sess.run(tf.local_variables_initializer())
-        sess.run(tf.global_variables_initializer())
-        coord = tf.train.Coordinator()
-        threads = tf.train.start_queue_runners(sess=sess,coord = coord)
-        train_steps = 10  
-        # Retrieve a single instance:  
-        try:  
-            while not coord.should_stop():  # 如果线程应该停止则返回True  
-                example,label = sess.run([image_batch,label_batch])  
-                print(example.shape,label)  
-  
-                train_steps -= 1  
-                print(train_steps)  
-                if train_steps <= 0:  
-                    coord.request_stop()    # 请求该线程停止  
-  
-        except tf.errors.OutOfRangeError:  
-            print ('Done training -- epoch limit reached')  
-        finally:  
-            # When done, ask the threads to stop. 请求该线程停止  
-            coord.request_stop()  
-            # And wait for them to actually do it. 等待被指定的线程终止  
-            coord.join(threads)      
-
-    
     
     
     
